3936ea891d9ac7d2ad2432f94c9d131504bfc81d
Valentin Horzinek <valentin.horzinek@outlook.com>
2023-01-21 17:50:17 +0100

    removed unused imports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { Config } from 'payload/config';
 import { CollectionConfig } from 'payload/types';
 import { BeforeChangeHook } from 'payload/dist/globals/config/types';
 import * as path from 'path';
-import * as fs from 'fs';
-import * as http from 'http';
 import axios from 'axios';
 
 import { getPlaiceholder } from 'plaiceholder';
